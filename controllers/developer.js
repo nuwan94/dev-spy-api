58430fff5a08689985b0d3ae33e7a5eebbc54afc
Nuwan94 <nuwansalawatta@gmail.com>
2020-05-13 21:42:47 +0530

    change response object

--- controllers/developer.js
+++ controllers/developer.js
@@ -4,13 +4,13 @@ const StackOverflow = require("../models/stackoverflow");
 module.exports = function(app) {
     app.get("/github", function(req, res) {
         GitHub.find({}, { _id: 0, __v: 0 }, (err, data) => {
-            res.send(data);
+            res.send(data[0]);
         });
     });
 
     app.get("/stackoverflow", function(req, res) {
         StackOverflow.find({}, { _id: 0, __v: 0 }, (err, data) => {
-            res.send(data);
+            res.send(data[0]);
         });
     });
 };
