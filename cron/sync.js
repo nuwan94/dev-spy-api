3e9e2e10293c4dfcca78ff5f46aaa5f0f2454ea9
Nuwan Alawatta <88030386+nala5539@users.noreply.github.com>
2022-01-24 02:02:20 +0530

    Update sync.js

--- cron/sync.js
+++ cron/sync.js
@@ -61,7 +61,7 @@ cron.schedule(cronInterval, async function() {
 
     await axios.get(process.env.MEDIUM_URI).then((response) => {
         const $ = cheerio.load(response.data);
-        followers_count = $('a:contains("Followers")').first().text().split(" ")[0];
+        followers_count = $('button:contains("Followers")').first().text().split(" ")[0];
         following_count = $('a:contains("Following")').first().text().split(" ")[0];
     });
 
